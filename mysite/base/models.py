0d3e198af7fc65440c4dd38eb4ae3ef4d17f3bb6
rommuloifrn <roomulo.cavalkant@outlook>
2023-06-27 19:48:59 -0300

    models atualizados (falta consertar alguns nomes)

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-
+from enum import Enum
 
 
 class Categoria(models.Model):
@@ -30,27 +29,37 @@ class Acao(models.Model):
     descricao = models.TextField()
     criada_em = models.DateTimeField(auto_now_add=True)
     modalidade = models.CharField(max_length=15)
-    local = CharField(max_length=50)
+    local = models.CharField(max_length=50)
     tema = models.CharField(max_length=20) ## Ñ seria melhor interpretar tema e categoria como se fosse uma só coisa? Ass. Rômulo
     max_volunt = models.IntegerField(null=True, blank=True)
     url = models.CharField(blank=True, max_length=200)
-    categoria = models.ForeignKey(Categoria, null=True, blank=True on_delete=models.SET_NULL)
+    categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
     inicio = models.DateTimeField()
     fim = models.DateTimeField()
 
     def __self__(self):
         return self.nome
 
-class Voluntario(models.Model):
-    status = Enum('Status')
-
-class Proprietario(models.Model):
-
 class Status(Enum):
     em_espera: 1
     aceito: 2
     rejeitado: 3
     participou: 4
+
+# user voluntaria em ação -> classe associativa voluntário é criada
+# baseado no Status da classe associativa Voluntário, as solicitações aparecem para os proprietários da ação
+# se um proprietário "mudar o status" (aceitar ou rejeitar a solicitação), é salvo como "Proprietário" na associativa Voluntário.
+
+# MUDAR OS NOMES DAS DUAS CLASSES ABAIXO (solicitação e resposta???)
+
+class Proprietario(models.Model):
+    proprietario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class Voluntario(models.Model):
+    voluntario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    status = Status
+    proprietario = models.ForeignKey(Proprietario, blank=True, null=True, on_delete=models.SET_NULL) # esse atributo representa a classe acima.
+
     
 
 class Foto(models.Model):
