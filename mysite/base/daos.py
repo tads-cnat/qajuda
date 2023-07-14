a609e16946829e08e9939dee6df37b72892c0c4b
Yuri Thairony <yurithairony@hotmail.com>
2023-07-14 18:13:45 -0300

    Detalhe da ação implementada com o banco

--- mysite/base/daos.py
+++ mysite/base/daos.py
@@ -1,1 +1,11 @@
+from .models import Acao
+
 # aqui estarão as classes DAO
+class AcaoDAO:
+    def acao_list(self):
+        lista_acao = Acao.objects.all()
+        return lista_acao
+        
+    def detalhe_acao(self, id):
+        acao = Acao.objects.get(pk=id)
+        return acao
--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+# app_name = 'base'
 urlpatterns = [
     path('', views.index),
     path('base/detail/', views.detail, name="detail"),
+    path('base/detalhe/<int:pk>/', views.DetalheViews.as_view(), name='detalhes'),
     path('base/response/', views.response, name="response"),
     path('base/voluntariar/', views.voluntariar, name="voluntariar"),
     path('base/pva/<int:acao_id>', views.pva, name="prop-viz-acao")
