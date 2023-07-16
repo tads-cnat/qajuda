a92dba00061fcaed304de19c566ce0963779c254
josevilanir <104863215+josevilanir@users.noreply.github.com>
2023-07-16 17:59:59 -0300

    Update urls.py

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('base/detail/', views.detail, name="detail"),
     path('base/response/', views.response, name="response"),
-    path('base/buscarAcao/', views.buscarAcao, name= "buscarAcao")
+    path('base/buscarAcao/', views.buscarAcao, name= "buscarAcao"),
     path('base/pva/<int:acao_id>', views.pva, name="prop-viz-acao")
 ]
