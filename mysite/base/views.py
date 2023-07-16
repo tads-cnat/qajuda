0eb4887022491e233553062369255cf39d504b6b
José Vilanir <vilanirneto@hotmail.com>
2023-07-16 17:36:15 -0300

    Implementação buscar ação (sem estilo)

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,42 +1,26 @@
-from django.shortcuts import get_object_or_404, render
-from django.views import View
-from django.views.generic.detail import DetailView
-
-from .daos import AcaoDAO
-from .models import Acao, Solicitacao
+from django.shortcuts import render, get_object_or_404
+from .models import Acao
 
 # Create your views here.
-
-# class IndexViews(View):
-#     def index(self, request):
-#         lista_acao = AcaoDAO.acao_list(request)
-#         solicitacao = Solicitacao.objects.filter(acao='lavarroupa').count()
-
-#         acao_destaque = lista_acao.order_by('-')
-#         context = {'lista_acao': lista_acao}
-#         return render(request, 'base/index.html', context)
-
 
 def index(request):
     return render(request, 'base/index.html')
 
-class DetalheViews(View):
-    def get(self, request, *args, **kwargs):
-        acao = get_object_or_404(Acao, pk=kwargs['pk'])
-        context = {'acao' : acao}
-        return render(request, 'base/detalhe.html', context)
-
-# def detail(request):
-#     return render(request, 'base/detail.html')
+def detail(request):
+    return render(request, 'base/detail.html')
 
 def response(request):
     return render(request, 'base/response.html')
 
-def voluntariar(request):
-    return render(request, 'base/voluntariar.html')
+def buscarAcao(request):
+    if 'search_query' in request.GET:
+        query = request.GET['search_query']
+        Acao = Acao.objects.filter(nome__icontains=query)
+    else:
+        Acao = []
+    return render(request, 'base/buscar_acao.html', {'acoes': Acao})
 
 def pva(request, acao_id):
     acao = get_object_or_404(Acao, pk=acao_id)
     context = {'acao':acao}
     return render(request, 'base/pva.html', context)
-
