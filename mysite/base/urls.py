4f587d95a6e6b8477e3e144e31cb7edd0f533e63
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-07-24 21:01:18 -0300

    Revert "41 reorganizacao backend"

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 from . import views
-# from .business import solicitar_participacao
-from .views import VoluntariarViews
 
-
-app_name = 'base'
 urlpatterns = [
     path('', views.index),
-    # path('', views.IndexViews.as_view(), name='index'),
-    path('base/detalhe/<int:pk>/', views.DetalheViews.as_view(), name='detalhes'),
+    path('base/detail/', views.detail, name="detail"),
     path('base/response/', views.response, name="response"),
-    path('base/voluntariar/<int:pk>/', views.VoluntariarViews.as_view(), name='voluntariar'),
-    path('voluntariar_em_acao/<int:acao_id>/', VoluntariarViews.voluntariar_em_acao, name='voluntariar_em_acao'),
-    path('buscar_acao/', views.BuscarAcaoViews.as_view(), name='buscar_acao'),
-
-    path('base/pva/<int:acao_id>', views.pva, name="prop-viz-acao"),
-    # roda bo botão
-    # path('solicitar-participacao/', solicitar_participacao, name='solicitar_participacao'),
+    path('base/buscarAcao/', views.buscarAcao, name= "buscarAcao"),
+    path('base/pva/<int:acao_id>', views.pva, name="prop-viz-acao")
 ]
