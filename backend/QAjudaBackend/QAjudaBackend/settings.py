1ff696213bbd952922301e02869ed6fb3855b036
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-10-25 14:04:34 -0300

    Update settings.py

--- backend/QAjudaBackend/QAjudaBackend/settings.py
+++ backend/QAjudaBackend/QAjudaBackend/settings.py
@@ -87,6 +87,14 @@ DATABASES = {
     }
 }
 
+# Database sqlite3
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
