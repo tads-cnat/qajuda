c93ceb0b3316eb15ba8337555349890fdb2b3e22
Yuri Thairony <yurithairony@hotmail.com>
2023-11-14 08:17:34 -0300

    Criando serviço e objeto colaborador

--- frontend/qajuda/src/app/views/cardcolaborador/cardcolaborador.component.ts
+++ frontend/qajuda/src/app/views/cardcolaborador/cardcolaborador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Colaborador } from 'src/app/models/colaborador';
 
 @Component({
   selector: 'app-cardcolaborador',
@@ -6,21 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./cardcolaborador.component.css']
 })
 export class CardcolaboradorComponent {
-  colaboradores: any[] = [];
+  colaboradores: Colaborador[] = [];
+
+  
 
   constructor() {
     // Crie exemplos de colaboradores e adicione à lista
-    this.colaboradores.push({ nome: 'Zezinho', idade: 30, telefone: '(84) 99456-7890', bairro: 'Planalto', aceito: false, recusado: false });
-    this.colaboradores.push({ nome: 'Bruninho', idade: 25, telefone: '(84) 99789-1230', bairro: 'Tirol', aceito: false, recusado: false });
-    this.colaboradores.push({ nome: 'Tia Lila', idade: 35, telefone: '(84) 99123-4560', bairro: 'Igapó', aceito: false, recusado: false });
-    this.colaboradores.push({ nome: 'Gringo', idade: 28, telefone: '(84) 99567-8901', bairro: 'Satélite', aceito: false, recusado: false });
-    this.colaboradores.push({ nome: 'Feli pão', idade: 32, telefone: '(84) 99890-1234', bairro: 'Tirol', aceito: false, recusado: false });
-    this.colaboradores.push({ nome: 'Coleguinha', idade: 35, telefone: '(84) 99890-1234', bairro: 'Tirol', aceito: false, recusado: false });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Zezinho', idade: 30, bairro: 'Planalto', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Bruninho', idade: 25, bairro: 'Tirol', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Tia Lila', idade: 35, bairro: 'Igapó', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Gringo', idade: 28, bairro: 'Satélite', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Feli pão', idade: 32, bairro: 'Tirol', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Coleguinha', idade: 35, bairro: 'Tirol', status: 'EM ESPERA' });
   }
 
-  responderSolicitacao(resposta: 'aceito' | 'rejeitado') {
-    if (resposta == 'aceito') { 
-      let aprovado = true; //chamar métodos aceitar_solicitação do backend
+  responderSolicitacao(resposta: 'ACEITO' | 'REJEITADO') {
+    if (resposta == 'ACEITO') { 
+        //chamar métodos aceitar_solicitação do backend
     } else {
       let recusado = true; //chamar métodos rejeitar_solicitação do backend
     }
