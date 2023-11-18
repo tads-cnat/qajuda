29636ce959a234d5bcc64dcbd5e572bd76d388ac
Yuri Thairony <yurithairony@hotmail.com>
2023-11-18 17:01:48 -0300

    Feat: Implementado rotas home e aprovar voluntario

--- frontend/qajuda/src/app/app-routing.module.ts
+++ frontend/qajuda/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { AprovarvoluntarioComponent } from './views/aprovarvoluntario/aprovarvoluntario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "aprovarvoluntario", component: AprovarvoluntarioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
