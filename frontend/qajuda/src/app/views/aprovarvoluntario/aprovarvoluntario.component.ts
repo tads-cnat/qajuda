b4a7b0cec1a46b440c891ef1662d215809e58072
Yuri Thairony <yurithairony@hotmail.com>
2023-11-28 10:49:42 -0300

    Style: reestilizando o CDU aprovar voluntario

--- frontend/qajuda/src/app/views/aprovarvoluntario/aprovarvoluntario.component.ts
+++ frontend/qajuda/src/app/views/aprovarvoluntario/aprovarvoluntario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Colaborador } from 'src/app/models/colaborador';
+
 
 @Component({
   selector: 'app-aprovarvoluntario',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./aprovarvoluntario.component.css']
 })
 export class AprovarvoluntarioComponent {
+  colaboradores: Colaborador[] = [];
 
+  
+
+  constructor() {
+    // Crie exemplos de colaboradores e adicione à lista
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Zezinho', idade: 30, bairro: 'Planalto', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Bruninho', idade: 25, bairro: 'Tirol', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Tia Lila', idade: 35, bairro: 'Igapó', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Gringo', idade: 28, bairro: 'Satélite', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Feli pão', idade: 32, bairro: 'Tirol', status: 'EM ESPERA' });
+    this.colaboradores.push({foto: 'sem foto' ,nome: 'Coleguinha', idade: 35, bairro: 'Tirol', status: 'EM ESPERA' });
+  }
 }
