da0d440e2072c13154db2228d58c0be57661e41b
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-12-05 22:40:25 -0300

    adaptador json CardDestaque

--- backend/QAjudaBackend/APIQAjuda/views.py
+++ backend/QAjudaBackend/APIQAjuda/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status, generics
 from .models import Colaborador_acao, Acao, Colaborador, Status
-from .serializers import ColaboradorAcaoSerializer, ColabAcaoSerialazer
+from .serializers import ColaboradorAcaoSerializer, AcaoSerializer, CardDestaqueSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import F, ExpressionWrapper, BooleanField
@@ -8,26 +8,29 @@ from django.db.models import F, ExpressionWrapper, BooleanField
 class SolicitacoesEmAbertoView(generics.ListAPIView):
     serializer_class = ColaboradorAcaoSerializer
 
-    def get_queryset(self):
-        acao_id = self.kwargs['acao_id']
-        return Colaborador_acao.objects.filter(acao_id=acao_id, solicitacao='E').select_related('colaborador')
+def get_queryset(self):
+    acao_id = self.kwargs['acao_id']
+    return Colaborador_acao.objects.filter(acao_id=acao_id, solicitacao='E').select_related('colaborador')
 
-    def perfom_update(self, serializer):
-        instance = serializer.instance
-        aceitar_solicitacao = serializer.validated_data.get('aceitar_solicitacao')
-        recusar_solicitacao = serializer.validated_data.get('recusar_solicitacao')
+def perfom_update(self, serializer):
+    instance = serializer.instance
+    aceitar_solicitacao = serializer.validated_data.get('aceitar_solicitacao')
+    recusar_solicitacao = serializer.validated_data.get('recusar_solicitacao')
 
-        if aceitar_solicitacao:
-            instance.solicitacao = 'A'
-            instance.save()
-            return Response({'status': 'Solicitação aceita'})
+    if aceitar_solicitacao:
+        instance.solicitacao = 'A'
+        instance.save()
+        return Response({'status': 'Solicitação aceita'})
 
-        if recusar_solicitacao:
-            instance.solicitacao = 'R'
-            instance.save()
-            return Response({'status': 'Solicitação recusada'})
+    if recusar_solicitacao:
+        instance.solicitacao = 'R'
+        instance.save()
+        return Response({'status': 'Solicitação recusada'})
 
-class ColaboradorAcaoViewSet(viewsets.ModelViewSet):
-    queryset = Colaborador_acao.objects.all()
-    serializer_class = SolicitacaoColaboradorSerializer
+class AcaoViewSet(viewsets.ModelViewSet):
+    queryset = Acao.objects.all()
+    serializer_class = AcaoSerializer
 
+class CardDestaqueSet(viewsets.ModelViewSet):
+    queryset = Colaborador_acao.objects.all().select_related('acao').select_related('colaborador').filter(solicitacao=Status.EM_ESPERA.value)
+    serializer_class = CardDestaqueSerializer
