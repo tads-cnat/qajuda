156d7dc3e15c7b108e136a5c599a23bd813e2414
Yuri Thairony <yurithairony@hotmail.com>
2023-12-29 10:59:46 -0300

    Feat: implementado parcialmente o buscar, falta rota

--- frontend/qajuda/src/app/services/acao.service.ts
+++ frontend/qajuda/src/app/services/acao.service.ts
@@ -7,7 +7,6 @@ import { Acao } from '../models/acao';
   providedIn: 'root'
 })
 export class AcaoService {
-  // url = 'http://localhost:3000/'; // trocar para 8000 quando usar api django
   url = 'http://localhost:8000/'; // url do back
   
   constructor(private http: HttpClient) { }
@@ -22,6 +21,7 @@ export class AcaoService {
 
   filtrarAcoesPorNome(nome: string): Observable<Acao[]> {
     const url = `${this.url}acao/?nome__icontains=${nome}`;
+    console.log(url)
     return this.http.get<Acao[]>(url);
   }
 }
