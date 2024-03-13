3024f4e713323f9c24150ef64eba57fcb721b3fc
Yuri Thairony <yurithairony@hotmail.com>
2024-03-13 20:55:50 -0300

    Feat: configurada preparação para login, falta armazenar token

--- frontend/src/app/views/TelaLogin/tela-login.component.ts
+++ frontend/src/app/views/TelaLogin/tela-login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-telalogin',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./tela-login.component.css']
 })
 export class TelaLoginComponent {
+  usuario = '';
+  senha = '';
+  showError = false;
 
+  constructor(private router:Router) {
+    
+  }
+
+  onSubmit() {
+    console.log('Usuário:', this.usuario);
+    console.log('Senha:', this.senha);
+    if (this.usuario === 'qajuda' && this.senha === 'qajuda') {
+      alert('Teu login deu certo, TOPPER!')
+      this.router.navigate(['/'])
+    } else {
+      this.showError = true;
+    }
+  }
 }
