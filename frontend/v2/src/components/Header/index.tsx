46d4412035a01d48b483e2c140c4c0b27aabf80d
pedro.maure@gmail.com <pedro.maure@gmail.com>
2024-03-20 19:20:35 -0300

    feat: implementa header

--- frontend/v2/src/components/Header/index.tsx
+++ frontend/v2/src/components/Header/index.tsx
@@ -1,40 +1,65 @@
-import Logo from "@/assets/img/logo/logo_header.svg"
-import { useNavigate } from "react-router-dom"
-import "./style.css"
-import SearchBar from "../SearchBar"
+import logo from "@/assets/img/logo/logo_header.svg";
+import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
+import SearchBar from "../SearchBar";
 
-
-
-function Header () {
-    const navigate = useNavigate()
-    function handleLogin () {
-        navigate("/login")
-    }
+function Header() {
+    const navigate = useNavigate();
 
     return (
-        <header>
-            <div className="header">
-                <div className="d-flex align-items-center py-2 justify-content-between">
-                    <div className="d-flex">
+        <nav className="navbar  header">
+
+            <div className="w-100 d-flex align-items-center justify-content-between header-container">
+                <div className="d-flex">
+
+                    <a
+                        className="navbar-brand"
+                        href="#"
+                    >
                         <img
-                            src={Logo}
+                            src={logo}
                             className="logo me-4"
                             alt="QAjuda"
                         />
-                        <SearchBar placeholder="Busque uma ação ou categoria"></SearchBar>
-                    </div>
-                    <div className="d-flex">
-                        <button
-                            className="btn btn-primary rounded rounded-pill"
-                            onClick={handleLogin}
+                    </a>
+                    <SearchBar placeholder="Busque alguma coisa" />
+                </div>
+                <div className="d-flex justify-content-evenly">
+                    <div className="d-flex align-items-center">
+
+                        <Link
+                            className="btn btn-primary me-3"
+                            to="/sobre"
                         >
-                            <i className="fa-solid fa-user pe-1"> Login </i>
-                        </button>
+                            Quem Somos
+                        </Link>
+                        <Link
+                            className="btn btn-primary me-3"
+                            to="/criar_acao"
+                        >
+                            Criar Ação
+                        </Link>
+                    </div>
+
+                    <div className="d-flex flex-column">
+                        <Link
+                            to="/cadastro"
+                            className="text-decoration-none text-white text-bold fw-bold"
+                        >
+                            Cadastro
+                        </Link>
+                        <Link
+                            to="/login"
+                            className="text-decoration-none text-white"
+                        >
+                            Login
+                        </Link>
+
                     </div>
                 </div>
             </div>
-        </header>
-    )
+        </nav>
+    );
 }
 
-export default Header
+export default Header;
