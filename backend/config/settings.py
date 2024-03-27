18083e6ae4fdd51adfff2861a718974caf3a5338
arthurmelo777 <meloarthurmelo@gmail.com>
2024-03-27 08:27:41 -0300

    Adiciona o código que não foi no ultimo commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
+    "http://localhost:5173",
     "http://localhost:8000",
     "https://seu-outro-dominio.com",
 ]
--- frontend/v2/src/pages/VisualizarAcao/index.tsx
+++ frontend/v2/src/pages/VisualizarAcao/index.tsx
@@ -1,13 +1,15 @@
 import './style.css'
 import { Acao } from '@/types/Acao'
 import AcaoService from '@/services/AcaoService'
-import UsuarioService from '@/services/UsuarioService'
+import ColaboradorService from '@/services/ColaboradorService'
 import { useEffect, useState } from 'react'
+import { Colaborador } from '@/types/Colaborador'
+
 //import { useParams } from 'node_modules/react-router-dom/dist/index'
 //import { toast } from 'node_modules/react-hot-toast/dist/index'
 
 function VisualizarAcao () {
-    const [userCriador, setUserCriador] = useState(null);
+    const [colaboradorCriador, setColaboradorCriador] = useState<Colaborador>();
     const [ acao, setAcao ] = useState<Acao>();
     //const { id } = useParams<{id: string}>();
 
@@ -16,6 +18,7 @@ function VisualizarAcao () {
     useEffect (() => {
         AcaoService.get(Number(id))
             .then(({ data }: { data: Acao }) => {
+                console.log('Dados: ', data)
                 setAcao(data)
             })
             .catch((err) => {
@@ -23,21 +26,24 @@ function VisualizarAcao () {
                 //toast.error('Erro ao encontrar ação.')
             })
     }, [id])
+    
+    console.log('Criador: ', acao?.criador)
 
-    useEffect(() => {
-        async function fetchUser() {
-            if (acao) {
-                const userData = await UsuarioService.get(acao.criador)
-                setUserCriador(userData)
-            }
-        }
-        fetchUser()
-    }, [acao?.criador])
+    useEffect (() => {
+        ColaboradorService.get(Number(acao?.criador))
+            .then(({ data }: { data: Colaborador }) => {
+                console.log('Dados criador: ', data)
+                setColaboradorCriador(data)
+            })
+            .catch((err) => {
+                console.error(err)
+                //toast.error('Erro ao encontrar ação.')
+            })
+    }, [id])
 
     return (
-        <div>
-            Visualizar Ação <br/> {acao}
-        </div>
+        <>
+        </>
     )
 }
 
