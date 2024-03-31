1fe8334906c274d8e9ab2d5d76275c7c7c72c663
Maure Andrade <pedro.maure@gmail.com>
2024-03-31 02:44:00 -0300

    feat: implementa componente de upload de foto

--- frontend/v2/src/components/Dropzone/ImageUploadField.tsx
+++ frontend/v2/src/components/Dropzone/ImageUploadField.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useMemo, useState } from "react";
+import FotoService from "@/services/FotoService";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
 import { Controller } from "react-hook-form";
 
@@ -37,66 +38,91 @@ interface ImageUploadFieldProps {
 }
 
 function ImageUploadField(props: ImageUploadFieldProps) {
-	const { placeholder, control, name } = props;
+	const { placeholder, name, control } = props;
 
-	const [myFiles, setMyFiles] = useState<FileWithPath[]>([]);
-
-	const onDrop = useCallback(
-		(acceptedFiles: FileWithPath[]) =>
-			setMyFiles([...myFiles, ...acceptedFiles]),
-		[myFiles]
-	);
-
-	const {
-		getRootProps,
-		getInputProps,
-		isFocused,
-		isDragAccept,
-		isDragReject,
-		acceptedFiles,
-		inputRef,
-	} = useDropzone({ accept: { "image/*": [] }, onDrop });
-
-	const removeFile = (file: FileWithPath) => () => {
-		const newFiles = [...myFiles];
-		newFiles.splice(newFiles.indexOf(file), 1);
-		setMyFiles(newFiles);
-	};
-
-	const removeAll = () => {
-		setMyFiles([]);
-		if (inputRef.current) inputRef.current.value = "";
-		acceptedFiles.length = 0;
-		acceptedFiles.splice(0, acceptedFiles.length);
-	};
-
-	const style = useMemo(
-		() => ({
-			...baseStyle,
-			...(isFocused ? focusedStyle : {}),
-			...(isDragAccept ? acceptStyle : {}),
-			...(isDragReject ? rejectStyle : {}),
-		}),
-		[isFocused, isDragAccept, isDragReject]
-	);
-
-	const files = acceptedFiles.map((file: FileWithPath) => <p>{file.path}</p>);
 	return (
 		<Controller
 			render={({ field: { onChange } }) => {
-				const onChangeValue = (e: any) => onChange("ok");
+				const [myFiles, setMyFiles] = useState<FileWithPath[]>([]);
+				const [disabled, setDisabled] = useState<boolean>(false);
+				const [isLoading, setIsLoading] = useState<boolean>(false);
+				const [uploadSuccess, setUploadSuccess] =
+					useState<boolean>(false);
+
+				const onDrop = useCallback(
+					(acceptedFiles: FileWithPath[]) =>
+						setMyFiles([...myFiles, ...acceptedFiles]),
+					[myFiles]
+				);
+
+				const {
+					getRootProps,
+					getInputProps,
+					isFocused,
+					isDragAccept,
+					isDragReject,
+					acceptedFiles,
+					inputRef,
+				} = useDropzone({
+					accept: { "image/*": [] },
+					onDrop,
+					disabled,
+				});
+
+				const removeAll = () => {
+					setMyFiles([]);
+					setIsLoading(false);
+					setUploadSuccess(false);
+					if (inputRef.current) inputRef.current.value = "";
+					acceptedFiles.length = 0;
+					acceptedFiles.splice(0, acceptedFiles.length);
+				};
+
+				const style = useMemo(
+					() => ({
+						...baseStyle,
+						...(isFocused ? focusedStyle : {}),
+						...(isDragAccept ? acceptStyle : {}),
+						...(isDragReject ? rejectStyle : {}),
+					}),
+					[isFocused, isDragAccept, isDragReject]
+				);
+
+				const files = acceptedFiles.map(
+					(file: FileWithPath, key: number) => (
+						<p key={key}>{file.path}</p>
+					)
+				);
+
+				useEffect(() => {
+					setDisabled(files.length > 0);
+				}, [files]);
+
+				function upload() {
+					setIsLoading(true);
+					setUploadSuccess(false);
+					let formDataBody = new FormData();
+					formDataBody.append("foto", myFiles[0]);
+					FotoService.post(formDataBody)
+						.then((res) => {
+							setIsLoading(false);
+							setUploadSuccess(true);
+							onChange(res.data.id);
+						})
+						.catch((err) => console.error(err));
+				}
+
 				return (
-					<div {...getRootProps({ className: "dropzone", style })}>
-						<input
-							{...getInputProps({
-								onChange: () => {
-									console.log("ok");
-								},
-							})}
-						/>
+					<div
+						{...getRootProps({
+							className: "dropzone",
+							style,
+						})}
+					>
+						<input {...getInputProps()} />
 						{files.length <= 0 ? (
 							<>
-								<p>Arraste e solte suas fotos aqui</p>
+								<p>{placeholder}</p>
 								<button
 									type="button"
 									className="btn btn-primary"
@@ -107,13 +133,30 @@ function ImageUploadField(props: ImageUploadFieldProps) {
 						) : (
 							<>
 								{files}
-								<button
-									type="button"
-									className="btn btn-outline-primary"
-									onClick={removeAll}
-								>
-									Escolher outra
-								</button>
+								<div className="d-flex">
+									<button
+										type="button"
+										className="btn btn-outline-danger me-3"
+										onClick={removeAll}
+									>
+										Remover foto
+									</button>
+									<button
+										className="btn btn-success"
+										onClick={upload}
+									>
+										{isLoading ? (
+											<div
+												className="spinner-border spinner-border-sm"
+												role="status"
+											/>
+										) : uploadSuccess ? (
+											<i className="fa-solid fa-check" />
+										) : (
+											<i className="fa-solid fa-cloud-arrow-up" />
+										)}
+									</button>
+								</div>
 							</>
 						)}
 					</div>
