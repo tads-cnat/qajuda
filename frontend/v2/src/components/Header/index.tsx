e2c1b173704faaafb377e351398cba713c8b03e8
Maure Andrade <pedro.maure@gmail.com>
2024-04-02 14:18:48 -0300

    fix: adiciona logo e corrige tratamento de evento

--- frontend/v2/src/components/Header/index.tsx
+++ frontend/v2/src/components/Header/index.tsx
@@ -1,65 +1,65 @@
-import logo from "@/assets/img/logo/logo_header.svg";
+import logo from "@/assets/img/logo/logo-invertido.png";
 import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
 import SearchBar from "../SearchBar";
 
 function Header() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return (
-        <nav className="navbar  header">
+	return (
+		<nav className="navbar header">
+			<div
+				className="w-100 d-flex align-items-center justify-content-between header-container"
+				style={{ maxHeight: "50px" }}
+			>
+				<div className="d-flex">
+					<a
+						className="navbar-brand"
+						href="#"
+					>
+						<img
+							src={logo}
+							className="logo me-4"
+							alt="QAjuda"
+							height="50px"
+						/>
+					</a>
+					<SearchBar placeholder="Busque uma ação ou categoria" />
+				</div>
+				<div className="d-flex justify-content-evenly">
+					<div className="d-flex align-items-center">
+						<Link
+							className="btn btn-primary me-3"
+							to="/sobre"
+						>
+							Quem Somos
+						</Link>
+						<Link
+							className="btn btn-primary me-3"
+							to="/criar-acao"
+						>
+							Criar Ação
+						</Link>
+					</div>
 
-            <div className="w-100 d-flex align-items-center justify-content-between header-container">
-                <div className="d-flex">
-
-                    <a
-                        className="navbar-brand"
-                        href="#"
-                    >
-                        <img
-                            src={logo}
-                            className="logo me-4"
-                            alt="QAjuda"
-                        />
-                    </a>
-                    <SearchBar placeholder="Busque uma ação ou categoria" />
-                </div>
-                <div className="d-flex justify-content-evenly">
-                    <div className="d-flex align-items-center">
-
-                        <Link
-                            className="btn btn-primary me-3"
-                            to="/sobre"
-                        >
-                            Quem Somos
-                        </Link>
-                        <Link
-                            className="btn btn-primary me-3"
-                            to="/criar_acao"
-                        >
-                            Criar Ação
-                        </Link>
-                    </div>
-
-                    <div className="d-flex flex-column">
-                        <Link
-                            to="/cadastro"
-                            className="text-decoration-none text-white text-bold fw-bold"
-                        >
-                            Cadastro
-                        </Link>
-                        <Link
-                            to="/login"
-                            className="text-decoration-none text-white"
-                        >
-                            Login
-                        </Link>
-
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+					<div className="d-flex flex-column">
+						<Link
+							to="/cadastro"
+							className="text-decoration-none text-white text-bold fw-bold"
+						>
+							Cadastro
+						</Link>
+						<Link
+							to="/login"
+							className="text-decoration-none text-white"
+						>
+							Login
+						</Link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
