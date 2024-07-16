badcd08a015375e3e603db1451cbee84fe4233cf
fxclsmg <fxclsmg@gmail.com>
2024-07-16 19:52:59 -0300

    Revert "Implementado classe de teste para o model ação"
    This reverts commit 29cc49995456ef66b14210a3271ad4f642f46c76.

--- backend/APIQAjuda/tests.py
+++ backend/APIQAjuda/tests.py
@@ -1,33 +1,3 @@
 from django.test import TestCase
-from .models import Acao, Foto, Categoria, Colaborador
-from datetime import datetime, date
 
-class AcaoTestCase(TestCase):
-    def setUp(self):
-        data_nascimento = date(1990, 1, 1)
-        self.foto = Foto.objects.create()  
-        self.categoria = Categoria.objects.create()  
-        self.colaborador = Colaborador.objects.create(data_nascimento=data_nascimento)  
-
-        self.acao = Acao.objects.create(
-            nome='Ação de Teste',
-            descricao='Descrição da ação de teste',
-            endereco='Endereço da ação',
-            inicio=datetime.now(),
-            foto=self.foto,
-            categoria=self.categoria,
-            criado_por=self.colaborador,
-        )
-
-    def test_str_representation(self):
-        self.assertEqual(str(self.acao), 'Ação de Teste')
-
-    def test_criado_em_auto_now_add(self):
-        self.assertIsNotNone(self.acao.criado_em)
-
-    def test_qtd_voluntarios_default_value(self):
-        self.assertEqual(self.acao.qtd_voluntarios, 0)
-
-    def test_nome_max_length(self):
-        max_length = self.acao._meta.get_field('nome').max_length
-        self.assertLessEqual(len(self.acao.nome), max_length)
+# Create your tests here.
