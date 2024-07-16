53101ccfb1084633c074174f0a99e087c134a689
Pedro Maure Frutuoso de Andrade <20221014040013@ifrn.local>
2024-07-16 11:03:48 -0300

    adiciona recebimento de variaveis de ambiente #241

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,15 +91,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qajudadb',
-        'USER': 'qajuda',
-        'PASSWORD': 'qajuda-password',
-        'HOST': 'qajuda-db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql' if os.getenv('POSTGRES_USER') else 'django.db.backends.sqlite3',
+        'NAME': os.getenv('POSTGRES_DB', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
