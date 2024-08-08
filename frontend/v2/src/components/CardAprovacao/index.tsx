499a20dddc94d7306110615382d9b471243652cb
arthurmelo777 <meloarthurmelo@gmail.com>
2024-08-08 08:31:18 -0300

    Corrige alterações no cartão de aprovação

--- frontend/v2/src/components/CardAprovacao/index.tsx
+++ frontend/v2/src/components/CardAprovacao/index.tsx
@@ -1,35 +1,26 @@
 import "./style.css";
 
 function CardAprovacao() {
-    return (
-        <>
-            <div
-                className="d-flex justify-content-between align-items-center cartao"
-            >
-                <div className="foto col-1">
-                    {/* FOTO AQUI */}
-                </div>
-                <div className="info col-3">
-                    PRIMEIRO NOME
-                </div>
-                <div className="info col-3">
-                    IDADE
-                </div>
-                <div className="info col-3">BAIRRO</div>
-                <div id="botao col-2">
-                    <button
-                        type="button"
-                        className="botao botao-aceitar"
-                        
-                    ></button>
-                    <button
-                        type="button"
-                        className="botao botao-recusar"
-                    ></button>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="d-flex justify-content-between align-items-center bg-primary rounded-pill py-3 px-1">
+				<div className="foto">{/* FOTO AQUI */}</div>
+				<div className="fs-5 ">Fulaninho das Quintas</div>
+				<div className="fs-5">27 anos</div>
+				<div className="fs-5">Alecrim</div>
+				<div className="d-flex g-2 pe-3">
+					<button
+						type="button"
+						className="botao botao-aceitar"
+					/>
+					<button
+						type="button"
+						className="botao botao-recusar"
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default CardAprovacao
+export default CardAprovacao;
